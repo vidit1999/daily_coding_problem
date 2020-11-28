906e11e70d9f1fe5bb97fe826b48b584ad8192d5
Vidit Sarkar <sarkarvidit71@gmail.com>
2020-11-28 15:33:38 +0530

    Added modified solution of 119

--- dailyCodingProblem119.cpp
+++ dailyCodingProblem119.cpp
@@ -9,17 +9,36 @@ For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9],
 one set of numbers that covers all these intervals is {3, 6}.
 */
 
-pair<int, int> minInterval(vector<pair<int, int>> intervals){
-	if(intervals.size() == 1) return intervals[0];
-	
-	sort(intervals.begin(), intervals.end());
-	return {intervals.front().second, intervals.back().first};
+vector<int> minSetInterval(vector<pair<int,int>> intervals){
+	if(intervals.empty()) return {};
+
+	sort(
+		intervals.begin(), intervals.end(),
+		[](pair<int, int>& p1, pair<int, int>& p2){
+			return p1.second < p2.second;
+		}
+	);
+
+	vector<int> ans = {intervals[0].second};
+	int prev = intervals[0].second;
+
+	for(auto it : intervals){
+		if(it.first > prev){
+			ans.push_back(it.second);
+			prev = it.second;
+		}
+	}
+
+	return ans;
 }
 
 void testFunc(vector<vector<pair<int, int>>> v){
 	for(auto intervals : v){
-		auto ans = minInterval(intervals);
-		cout << ans.first << ", " << ans.second << "\n";
+		auto ans = minSetInterval(intervals);
+		for(int i : ans){
+			cout << i << "\n";
+		}
+		cout << "\n";
 	}
 }
 
@@ -27,8 +46,10 @@ void testFunc(vector<vector<pair<int, int>>> v){
 int main(){
 	testFunc({
 		{{0, 3}, {2, 6}, {3, 4}, {6, 9}},
+		{{0, 2}, {3, 5}, {7, 9}},
+		{{0, 5}, {1, 4}, {2, 3}},
 		{{1, 2}, {1, 3}, {3, 4}, {4, 5}, {3, 6}},
-		{{1, 3}},
+		{{1, 3}}
 	});
 	return 0;
 }
