d272891488f53dcc4b5ec0eec88dea7a880ae9e9
Vidit Sarkar <sarkarvidit71@gmail.com>
2020-12-01 23:14:29 +0530

    Added solutions of 552, 553

---
+++ dailyCodingProblem553.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define N 3
+#define M 3
+using namespace std;
+
+/*
+You are given an N by M 2D matrix of lowercase letters.
+Determine the minimum number of columns that can be
+removed to ensure that each row is ordered from top
+to bottom lexicographically. That is, the letter at
+each column is lexicographically later as you go down
+each row. It does not matter whether each row itself
+is ordered lexicographically.
+
+For example, given the following table:
+
+cba
+daf
+ghi
+
+This is not ordered because of the a in the center.
+We can remove the second column to make it ordered:
+
+ca
+df
+gi
+
+So your function should return 1,
+since we only needed to remove 1 column.
+
+As another example, given the following table:
+
+abcdef
+
+Your function should return 0,
+since the rows are already ordered (there's only one row).
+
+As another example, given the following table:
+
+zyx
+wvu
+tsr
+
+Your function should return 3,
+since we would need to remove all the columns to order it.
+*/
+
+int removeCount(char mat[N][M]){
+    int count  = 0;
+
+    for(int i=0; i<M; i++){
+        for(int j=1; j<N; j++){
+            if(mat[j][i] <= mat[j-1][i]){
+                count++;
+                break;
+            }
+        }
+    }
+
+    return count;
+}
+
+// main function
+int main(){
+    /* 
+		{{'c','b','a'},
+        {'d','a','f'},
+        {'g','h','i'}}
+
+        {{'a','b','c','d','e','f'}},
+        
+        {{'z','y','x'},
+        {'w','v','u'},
+        {'t','s','r'}}
+    */
+
+	char mat[N][M] = {
+		{'z','y','x'},
+        {'w','v','u'},
+        {'t','s','r'}
+	};
+
+    cout << removeCount(mat) << "\n";
+	return 0;
+}
