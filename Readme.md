b2e7d566d0c33cf7ff35317df6c957f2c6a61f60
Vidit Sarkar <sarkarvidit71@gmail.com>
2021-01-22 00:12:24 +0530

    Added solution of 776

--- Readme.md
+++ Readme.md
@@ -13381,3 +13381,24 @@ For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 [Solution](dailyCodingProblem775.cpp)
 
 ---
+
+
+#### Problem No. 776
+
+```
+There are N prisoners standing in a circle, waiting to be executed.
+The executions are carried out starting with the kth person,
+and removing every successive kth person going clockwise
+until there is no one left.
+
+Given N and k, write an algorithm to determine where a
+prisoner should stand in order to be the last survivor.
+
+For example, if N = 5 and k = 2, the order of executions
+would be [2, 4, 1, 5, 3], so you should return 3.
+
+Bonus: Find an O(log N) solution if k = 2.
+```
+[Solution](dailyCodingProblem776.cpp)
+
+---
---
+++ dailyCodingProblem776.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+There are N prisoners standing in a circle, waiting to be executed.
+The executions are carried out starting with the kth person,
+and removing every successive kth person going clockwise
+until there is no one left.
+
+Given N and k, write an algorithm to determine where a
+prisoner should stand in order to be the last survivor.
+
+For example, if N = 5 and k = 2, the order of executions
+would be [2, 4, 1, 5, 3], so you should return 3.
+
+Bonus: Find an O(log N) solution if k = 2.
+*/
+
+int josephus(int n, int k){
+	if(n == 1) return 1;
+	return (josephus(n-1, k) + k-1)%n + 1;
+}
+
+int josephus2(int n){
+	int p = 1;
+	
+	while(p <= n) p *= 2;
+	
+	return 2*n - p + 1;
+}
+
+// main function
+int main(){
+	cout << josephus(14, 2) << "\n";
+	cout << josephus2(14) << "\n";
+	cout << josephus(5, 2) << "\n";
+	cout << josephus2(5) << "\n";
+	return 0;
+}
