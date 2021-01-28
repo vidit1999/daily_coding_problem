926b17918bc779a75a491a5bc5f96b2f9cf62f5e
Vidit Sarkar <sarkarvidit71@gmail.com>
2021-01-29 00:00:16 +0530

    Added solution of 783

--- Readme.md
+++ Readme.md
@@ -13515,3 +13515,17 @@ Given a list of paper citations of a researcher, calculate their h-index.
 [Solution](dailyCodingProblem782.cpp)
 
 ---
+
+
+#### Problem No. 783
+
+```
+Given an array of numbers, find the length of the longest increasing
+subsequence in the array. The subsequence does not necessarily have to be contiguous.
+
+For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15],
+the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
+```
+[Solution](dailyCodingProblem783.cpp)
+
+---
---
+++ dailyCodingProblem783.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Given an array of numbers, find the length of the longest increasing
+subsequence in the array. The subsequence does not necessarily have to be contiguous.
+
+For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15],
+the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
+*/
+
+int longestIncresingSubSeq(int arr[], int n){
+	int lis[n];
+	
+	for(int i=0; i<n; i++){
+		lis[i] = 1;
+		for(int j=0; j<i; j++){
+			if(arr[i] > arr[j]){
+				lis[i] = max(lis[i], lis[j]+1);
+			}
+		}
+	}
+	
+	return *max_element(lis, lis+n);
+}
+
+// main function
+int main(){
+	int arr[] = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout << longestIncresingSubSeq(arr, n) << "\n";
+	return 0;
+}
